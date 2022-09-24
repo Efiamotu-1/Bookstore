b2972ccdc17b31efcb49b652fc32040d5c1162a2
Efiamotu-1 <musahabeebefy10@gmail.com>
2022-09-24 05:54:48 +0100

    new change

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -5,9 +5,6 @@ const baseUrl = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/book
 
 const initialState = [];
 
-// axios.post('https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/')
-//   .then((response) => console.log(response.data));
-
 // Generates pending, fulfilled and rejected action types
 export const fetchBooks = createAsyncThunk('books/fetchBooks', () => axios.get(baseUrl)
   .then((response) => {
